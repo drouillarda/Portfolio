be5a02d209b47260470a9405c4e90b73c1bea8a2
alex.drouillard <alex.drouillard.9@hotmail.com>
2024-06-07 16:47:02 -0400

    Nav and Vader added

--- components/ui/Nav/Nav.tsx
+++ components/ui/Nav/Nav.tsx
@@ -6,7 +6,17 @@ export default async function Navbar() {
   
   return (
     <nav className="nav">
-        <p>Take me to the Light Side</p>
+        <article>
+            <Link href="/light-side">
+                <p>Take me to the Light Side</p>
+            </Link>
+        </article>
+        <article className="nav__end">
+            <p>My Resume</p>
+            <p>My Values</p>
+            <p>My Hobbies</p>
+            <p>My Projects</p>
+        </article>
     </nav>
   );
 }
--- src/app/dark-side/page.tsx
+++ src/app/dark-side/page.tsx
@@ -1,4 +1,5 @@
 import '../../../styles/_global.scss';
+import Image from 'next/image';
 import Navbar from '../../../components/ui/Nav/Nav';
 import "./darkside.scss";
 
@@ -7,8 +8,14 @@ export default function DarkSide () {
     return (
         <main className='darkSide'>
             <Navbar />
-            <section>
-                <h1>Hello World</h1>
+            <section className='darkSide__img'>
+                <Image 
+                src="/Vader.png"
+                alt="Vader"
+                width={384}
+                height={214.5}
+                />
+                <h1 >Welcome to my Portfolio</h1>
             </section>
         </main>
     )
