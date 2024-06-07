6b91c18251f2061f1012d3d4718abceb6a3bf6a9
alex.drouillard <alex.drouillard.9@hotmail.com>
2024-06-07 13:12:37 -0400

    Logos added correctly

--- src/app/components/ui/ChooseYourSide/ChooseYourSide.tsx
+++ src/app/components/ui/ChooseYourSide/ChooseYourSide.tsx
@@ -10,29 +10,29 @@ export default async function ChooseYourSide() {
     <main className="chooseYourSide">
       <section className="chooseYourSide__inner">
         <article className="chooseYourSide__inner--light">
+          <h2>Light Side</h2>
           <div className="chooseYourSide__inner--light--absolute">
-          <Image
-            src="/../images/JediLogo.png"
+            <Image
+            src="/JediLogo.png"
             alt="Jedi Logo"
             width={300}
             height={300}
           />
           </div>
-          <h2>Light Side</h2>
         </article>
         <article className="chooseYourSide__inner--absolute">
           <h1>Choose Your Side</h1>
         </article>
         <article className="chooseYourSide__inner--dark">
+          <h2>Dark Side</h2>
           <div className="chooseYourSide__inner--light--absolute">
             <Image
-              src="/../images/Sith-Emblem.png"
+              src="/Sith-Emblem.png"
               alt="Sith Logo"
-              width={300}
+              width={600}
               height={300}
             />
             </div>
-          <h2>Dark Side</h2>
         </article>
       </section>
     </main>
