b74249970a4a6f763e4d7e76f6a2f260590c2d10
alex.drouillard <alex.drouillard.9@hotmail.com>
2024-06-07 13:05:38 -0400

    logos added

--- src/app/components/ui/ChooseYourSide/ChooseYourSide.tsx
+++ src/app/components/ui/ChooseYourSide/ChooseYourSide.tsx
@@ -1,3 +1,6 @@
+import { Link } from "react-router-dom";
+import Image from "next/image";
+
 import "./ChooseYourSide.scss"
 
 
@@ -7,12 +10,28 @@ export default async function ChooseYourSide() {
     <main className="chooseYourSide">
       <section className="chooseYourSide__inner">
         <article className="chooseYourSide__inner--light">
+          <div className="chooseYourSide__inner--light--absolute">
+          <Image
+            src="/../images/JediLogo.png"
+            alt="Jedi Logo"
+            width={300}
+            height={300}
+          />
+          </div>
           <h2>Light Side</h2>
         </article>
         <article className="chooseYourSide__inner--absolute">
           <h1>Choose Your Side</h1>
         </article>
         <article className="chooseYourSide__inner--dark">
+          <div className="chooseYourSide__inner--light--absolute">
+            <Image
+              src="/../images/Sith-Emblem.png"
+              alt="Sith Logo"
+              width={300}
+              height={300}
+            />
+            </div>
           <h2>Dark Side</h2>
         </article>
       </section>
